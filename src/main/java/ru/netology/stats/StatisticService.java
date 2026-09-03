c55c0247ca1a11f0502c36462ef05c7f83c66074
toUffie <touffie@me.com>
2026-09-04 00:51:09 +0200

    JoCoCo add and set

--- src/main/java/ru/netology/stats/StatisticService.java
+++ src/main/java/ru/netology/stats/StatisticService.java
@@ -1,3 +1,4 @@
+package ru.netology.stats;
 public class StatisticService {
     public long findMax(long[] incomes) {
         long currentMax = incomes[0];
--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -1,3 +1,4 @@
+package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,17 @@ public class StatisticServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxFromMidle() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 30, 6, 11, 11, 12};
+        long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
 
